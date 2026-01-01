6a8ee24a3b932cfe0b9d63d0560c5cfc13b88fa1
Lars Rustand <rustand.lars@gmail.com>
2026-01-01 08:37:53 +0100

    Avoid loading outdated bytecompiled lisp files

--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el --- My Emacs config     -*- lexical-binding: t; -*-
+;;; early-init.el --- My Emacs config     -*- lexical-binding: t; no-byte-compile: t; -*-
 
 ;; Disable junk
 (push '(menu-bar-lines . 0) default-frame-alist)
@@ -12,3 +12,5 @@
 (setq gc-cons-threshold 500000000) ; Set to 500MB
 (setq garbage-collection-messages t)
 (setq package-enable-at-startup nil)
+
+(setq load-prefer-newer t) ; Load source file if byte-compiled is outdated
